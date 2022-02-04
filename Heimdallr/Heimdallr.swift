91b978ef463f5a1c2b23cb44d6e229b192a73185
Yoel Jimenez del Valle <yoel07@gmail.com>
2022-02-04 06:36:50 -0500

    check for a sho param to do the right encode and removed it becase sso is not needed and remove the comments

--- Heimdallr/Heimdallr.swift
+++ Heimdallr/Heimdallr.swift
@@ -129,9 +129,10 @@ public let HeimdallrErrorNotAuthorized = 2
         var request = URLRequest(url: tokenURL)
 
         var parameters = grant.parameters
-        print("parameters:", parameters)
+        let isSSO = parameters["sso"]
+        parameters["sso"] = nil
         if let credentials = credentials {
-            if parameters["grant_type"] == nil || parameters["grant_type"] != "password"  {
+            if isSSO == nil {
                 if let secret = credentials.secret {
                     request.setHTTPAuthorization(.basicAuthentication(username: credentials.id, password: secret))
                 } else {
@@ -151,13 +152,13 @@ public let HeimdallrErrorNotAuthorized = 2
 
         request.httpMethod = "POST"
         
-        if parameters["grant_type"] != nil {
-            print("params is not nil")
+        if isSSO != nil {
+//            print("params is not nil")
             request.setValue("application/json", forHTTPHeaderField: "Content-Type")
             let data = try? JSONSerialization.data(withJSONObject: parameters, options: .fragmentsAllowed)
             request.httpBody = data//setHTTPBody(parameters: data)
         }else {
-            print("params is nil")
+//            print("params is nil")
             request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
             request.setHTTPBody(parameters: parameters as [String: AnyObject])
         }
